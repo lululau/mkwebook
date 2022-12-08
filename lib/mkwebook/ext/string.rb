fd2619821666fc691601a5ae67cb56147f7d1d86
Liu Xiang <liuxiang@ktjr.com>
2022-12-08 12:56:39 +0800

    Add uri related ext methods for String

--- lib/mkwebook/ext/string.rb
+++ lib/mkwebook/ext/string.rb
@@ -10,4 +10,33 @@ class String
   def f
     expa
   end
+
+  def normalize_file_path(force_extname = nil)
+    uri = URI.parse(self)
+    file_path = uri.path[1..]
+    extname = File.extname(file_path)
+    basename = File.basename(file_path, extname)
+    basename += "_#{Digest::MD5.hexdigest(uri.query)}" if uri.query.present?
+    extname = force_extname if force_extname && extname.empty?
+    File.join(File.dirname(file_path), basename + extname)
+  end
+
+  def normalize_uri(force_extname = nil)
+    uri = URI.parse(self)
+    file_path = uri.path[1..]
+    extname = File.extname(file_path)
+    basename = File.basename(file_path, extname)
+    basename += "_#{Digest::MD5.hexdigest(uri.query)}" if uri.query.present?
+    extname = force_extname if force_extname && extname.empty?
+    file_path = File.join(File.dirname(file_path), basename + extname)
+    if uri.fragment.present?
+      file_path += "##{uri.fragment}"
+    else
+      file_path
+    end
+  end
+
+  def relative_path_from(base)
+    Pathname.new(base).relative_path_from(Pathname.new(self)).to_s
+  end
 end
